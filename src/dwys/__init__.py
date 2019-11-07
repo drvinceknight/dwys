7229cd7994544791311f573abc7ca1443284276d
Vince Knight <vincent.knight@gmail.com>
2019-11-07 09:25:51 +0000

    Checker works for python.

--- src/dwys/__init__.py
+++ src/dwys/__init__.py
@@ -1,4 +1,7 @@
+import difflib
+import tempfile
 import re
+import subprocess
 
 def parse(string, in_pattern, out_pattern=None):
     """
@@ -11,3 +14,34 @@ def parse(string, in_pattern, out_pattern=None):
     else:
         output_code = None
     return input_code, output_code
+
+def diff(input_code, output_code, execution_command, input_filename=None,
+        output_filename=None, expected_output_filename=None):
+    """
+    Run the input code with the specified <execuation_command>, and generates a
+    diff with the output code.
+    """
+    if input_filename is None:
+        input_filename = tempfile.NamedTemporaryFile().name
+
+    if output_filename is None:
+        output_filename = tempfile.NamedTemporaryFile().name
+
+    if expected_output_filename is None:
+        expected_output_filename = tempfile.NamedTemporaryFile().name
+
+    with open(input_filename, "w") as file_to_write:
+        for code_snippet in input_code:
+            file_to_write.write(code_snippet)
+
+    with open(expected_output_filename, "w") as file_to_write:
+        for code_snippet in output_code:
+            file_to_write.write(code_snippet)
+
+    completed_process = subprocess.run([execution_command, input_filename], capture_output=True)
+    output = completed_process.stdout.decode("utf-8")
+
+    with open(expected_output_filename, "r") as file_to_read:
+        expected_output = file_to_read.read()
+
+    return difflib.unified_diff(expected_output, output, n=0)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -24,16 +24,16 @@ Output starts:
 10
 Output end.
     """
-    in_pattern = re.compile(r"Code starts:\n(.*?)\nCode end", re.DOTALL)
-    out_pattern = re.compile(r"Output starts:\n(.*?)\nOutput end", re.DOTALL)
+    in_pattern = re.compile(r"Code starts:\n(.*?)Code end", re.DOTALL)
+    out_pattern = re.compile(r"Output starts:\n(.*?)Output end", re.DOTALL)
 
     input_code, output_code = dwys.parse(
             string=text,
             in_pattern=in_pattern,
             out_pattern=out_pattern,
             )
-    assert input_code == ["a = 4 + 4\nb = a + 3", "weights = [12, 15, 10]\nfor weight in weights:\n    print(weight)"]
-    assert output_code == ['12\n15\n10']
+    assert input_code == ["a = 4 + 4\nb = a + 3\n", "weights = [12, 15, 10]\nfor weight in weights:\n    print(weight)\n"]
+    assert output_code == ['12\n15\n10\n']
 
 def test_parser_with_latex_delimiters():
     text = r"""
@@ -58,46 +58,13 @@ for weight in weights:
 10
 \end{pyout}
     """
-    in_pattern = re.compile(r"\\begin\{pyin\}\n(.*?)\n\\end\{pyin\}", re.DOTALL)
-    out_pattern = re.compile(r"\\begin\{pyout\}\n(.*?)\n\\end\{pyout\}", re.DOTALL)
+    in_pattern = re.compile(r"\\begin\{pyin\}\n(.*?)\\end\{pyin\}", re.DOTALL)
+    out_pattern = re.compile(r"\\begin\{pyout\}\n(.*?)\\end\{pyout\}", re.DOTALL)
 
     input_code, output_code = dwys.parse(
             string=text,
             in_pattern=in_pattern,
             out_pattern=out_pattern,
             )
-    assert input_code == ["a = 4 + 4\nb = a + 3", "weights = [12, 15, 10]\nfor weight in weights:\n    print(weight)"]
-    assert output_code == ['12\n15\n10']
-def test_parser_with_basic_delimiters():
-    text = """
-Here is some text with some python code:
-
-Code starts:
-a = 4 + 4
-b = a + 3
-Code end.
-
-Here is some more code:
-
-Code starts:
-weights = [12, 15, 10]
-for weight in weights:
-    print(weight)
-Code end.
-
-Output starts:
-12
-15
-10
-Output end.
-    """
-    in_pattern = re.compile(r"Code starts:\n(.*?)\nCode end", re.DOTALL)
-    out_pattern = re.compile(r"Output starts:\n(.*?)\nOutput end", re.DOTALL)
-
-    input_code, output_code = dwys.parse(
-            string=text,
-            in_pattern=in_pattern,
-            out_pattern=out_pattern,
-            )
-    assert input_code == ["a = 4 + 4\nb = a + 3", "weights = [12, 15, 10]\nfor weight in weights:\n    print(weight)"]
-    assert output_code == ['12\n15\n10']
+    assert input_code == ["a = 4 + 4\nb = a + 3\n", "weights = [12, 15, 10]\nfor weight in weights:\n    print(weight)\n"]
+    assert output_code == ['12\n15\n10\n']
