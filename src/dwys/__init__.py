0451753d4deaabb62a78b5b8fa133df27bf12920
Vince Knight <vincent.knight@gmail.com>
2019-11-07 09:31:16 +0000

    Refactor code to only need 1 temp file.

--- src/dwys/__init__.py
+++ src/dwys/__init__.py
@@ -15,8 +15,7 @@ def parse(string, in_pattern, out_pattern=None):
         output_code = None
     return input_code, output_code
 
-def diff(input_code, output_code, execution_command, input_filename=None,
-        output_filename=None, expected_output_filename=None):
+def diff(input_code, expected_output_code, execution_command, input_filename=None,):
     """
     Run the input code with the specified <execuation_command>, and generates a
     diff with the output code.
@@ -24,24 +23,11 @@ def diff(input_code, output_code, execution_command, input_filename=None,
     if input_filename is None:
         input_filename = tempfile.NamedTemporaryFile().name
 
-    if output_filename is None:
-        output_filename = tempfile.NamedTemporaryFile().name
-
-    if expected_output_filename is None:
-        expected_output_filename = tempfile.NamedTemporaryFile().name
-
     with open(input_filename, "w") as file_to_write:
         for code_snippet in input_code:
-            file_to_write.write(code_snippet)
-
-    with open(expected_output_filename, "w") as file_to_write:
-        for code_snippet in output_code:
             file_to_write.write(code_snippet)
 
     completed_process = subprocess.run([execution_command, input_filename], capture_output=True)
     output = completed_process.stdout.decode("utf-8")
 
-    with open(expected_output_filename, "r") as file_to_read:
-        expected_output = file_to_read.read()
-
-    return difflib.unified_diff(expected_output, output, n=0)
+    return difflib.unified_diff("\n".join(expected_output_code), output, n=0)
--- tests/test_checker.py
+++ tests/test_checker.py
@@ -3,12 +3,12 @@ import dwys
 def test_diff_with_no_diff_python():
     input_code = ["a = 4 + 4\nb = a + 3\n", "weights = [12, 15, 10]\nfor weight in weights:\n    print(weight)\n"]
     output_code = ['12\n15\n10\n']
-    assert list(dwys.diff(input_code=input_code, output_code=output_code, execution_command="python")) == []
+    assert list(dwys.diff(input_code=input_code, expected_output_code=output_code, execution_command="python")) == []
 
 def test_diff_with_diff_python():
     input_code = ["a = 4 + 4\nb = a + 3\n", "weights = [12, 15, 10]\nfor weight in weights:\n    print(weight)\n"]
     output_code = ['12\n151\n10\n']
-    assert list(dwys.diff(input_code=input_code, output_code=output_code,
+    assert list(dwys.diff(input_code=input_code, expected_output_code=output_code,
         execution_command="python")) == ['--- \n', '+++ \n', '@@ -6 +5,0 @@\n', '-1']
 
 # def test_diff_with_no_diff_R():
