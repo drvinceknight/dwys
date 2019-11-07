98d8bc4765ad582ea04e48021315c476e78ffad4
Vince Knight <vincent.knight@gmail.com>
2019-11-07 10:55:49 +0000

    Plug in to travis (#1)
    * Add black.
    
    * Run isort.
    
    * Add R to travis.

--- src/dwys/__init__.py
+++ src/dwys/__init__.py
@@ -1,7 +1,8 @@
 import difflib
-import tempfile
 import re
 import subprocess
+import tempfile
+
 
 def parse(string, in_pattern, out_pattern=None):
     """
@@ -15,7 +16,8 @@ def parse(string, in_pattern, out_pattern=None):
         output_code = None
     return input_code, output_code
 
-def diff(input_code, expected_output_code, execution_command, input_filename=None,):
+
+def diff(input_code, expected_output_code, execution_command, input_filename=None):
     """
     Run the input code with the specified <execuation_command>, and generates a
     diff with the output code.
@@ -27,8 +29,10 @@ def diff(input_code, expected_output_code, execution_command, input_filename=Non
         for code_snippet in input_code:
             file_to_write.write(code_snippet)
 
-    completed_process = subprocess.run([execution_command, input_filename], capture_output=True)
-    assert completed_process.stderr == b'', "Syntax error in code"
+    completed_process = subprocess.run(
+        [execution_command, input_filename], capture_output=True
+    )
+    assert completed_process.stderr == b"", "Syntax error in code"
     output = completed_process.stdout.decode("utf-8")
 
     # TODO Make the diff usable.
--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,39 +1,92 @@
-import dwys
 import pytest
 
+import dwys
+
+
 def test_diff_with_no_diff_python():
-    input_code = ["a = 4 + 4\nb = a + 3\n", "weights = [12, 15, 10]\nfor weight in weights:\n    print(weight)\n"]
-    output_code = ['12\n15\n10\n']
-    assert list(dwys.diff(input_code=input_code, expected_output_code=output_code, execution_command="python")) == []
+    input_code = [
+        "a = 4 + 4\nb = a + 3\n",
+        "weights = [12, 15, 10]\nfor weight in weights:\n    print(weight)\n",
+    ]
+    output_code = ["12\n15\n10\n"]
+    assert (
+        list(
+            dwys.diff(
+                input_code=input_code,
+                expected_output_code=output_code,
+                execution_command="python",
+            )
+        )
+        == []
+    )
+
 
 def test_diff_with_diff_python():
-    input_code = ["a = 4 + 4\nb = a + 3\n", "weights = [12, 15, 10]\nfor weight in weights:\n    print(weight)\n"]
-    output_code = ['12\n151\n10\n']
-    assert list(dwys.diff(input_code=input_code, expected_output_code=output_code,
-        execution_command="python")) == ['--- \n', '+++ \n', '@@ -6 +5,0 @@\n', '-1']
+    input_code = [
+        "a = 4 + 4\nb = a + 3\n",
+        "weights = [12, 15, 10]\nfor weight in weights:\n    print(weight)\n",
+    ]
+    output_code = ["12\n151\n10\n"]
+    assert list(
+        dwys.diff(
+            input_code=input_code,
+            expected_output_code=output_code,
+            execution_command="python",
+        )
+    ) == ["--- \n", "+++ \n", "@@ -6 +5,0 @@\n", "-1"]
+
 
 def test_diff_with_syntax_error_python():
-    input_code = ["a = 4 + 4\nb = a + 3\n", "weights = (12, 15, 10]\nfor weight in weights:\n    print(weight)\n"]
-    output_code = ['12\n151\n10\n']
+    input_code = [
+        "a = 4 + 4\nb = a + 3\n",
+        "weights = (12, 15, 10]\nfor weight in weights:\n    print(weight)\n",
+    ]
+    output_code = ["12\n151\n10\n"]
     with pytest.raises(AssertionError):
-        list(dwys.diff(input_code=input_code, expected_output_code=output_code,
-        execution_command="python"))
+        list(
+            dwys.diff(
+                input_code=input_code,
+                expected_output_code=output_code,
+                execution_command="python",
+            )
+        )
+
 
 def test_diff_with_no_diff_R():
     input_code = ["a <- 4 + 4\nb <- a + 3\nprint(a + b)\n"]
-    output_code = ['[1] 19\n']
-    assert list(dwys.diff(input_code=input_code,
-        expected_output_code=output_code, execution_command="Rscript")) == []
+    output_code = ["[1] 19\n"]
+    assert (
+        list(
+            dwys.diff(
+                input_code=input_code,
+                expected_output_code=output_code,
+                execution_command="Rscript",
+            )
+        )
+        == []
+    )
+
 
 def test_diff_with_diff_R():
     input_code = ["a <- 4 + 4\nb <- a + 3\nprint(a + b)\n"]
-    output_code = ['[1] 191\n']
-    assert list(dwys.diff(input_code=input_code,
-        expected_output_code=output_code, execution_command="Rscript")) == ['--- \n', '+++ \n', '@@ -7 +6,0 @@\n', '-1']
+    output_code = ["[1] 191\n"]
+    assert list(
+        dwys.diff(
+            input_code=input_code,
+            expected_output_code=output_code,
+            execution_command="Rscript",
+        )
+    ) == ["--- \n", "+++ \n", "@@ -7 +6,0 @@\n", "-1"]
+
 
 def test_diff_with_syntax_error_R():
     input_code = ["a <- 4 + 4\nb <- a + 3\nprint{a + b)\n"]
-    output_code = ['[1] 19\n']
+    output_code = ["[1] 19\n"]
     with pytest.raises(AssertionError):
-        list(dwys.diff(input_code=input_code,
-        expected_output_code=output_code, execution_command="Rscript")) == []
+        list(
+            dwys.diff(
+                input_code=input_code,
+                expected_output_code=output_code,
+                execution_command="Rscript",
+            )
+        ) == []
