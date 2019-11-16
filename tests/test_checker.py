6744ec9d11f6f543262775d7fb26ee689b1895ca
Vince Knight <vincent.knight@gmail.com>
2019-11-16 15:38:46 +0000

    Change outputs to include expected. (#9)

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -5,16 +5,15 @@ import dwys
 
 def test_diff_with_no_diff_python():
     input_code = [
-        "a = 4 + 4\nb = a + 3\n",
+        "a = 4 + 4\nb = a + 3\nprint(b)\n",
         "weights = [12, 15, 10]\nfor weight in weights:\n    print(weight)\n",
     ]
-    expected_output_code = ["12\n15\n10\n"]
-    diff, output_code = dwys.diff(
+    expected_output_code = ["11\n12\n15\n10"]
+    diff, output_code, expected_output = dwys.diff(
         input_code=input_code,
         expected_output_code=expected_output_code,
         execution_command="python",
     )
-    assert list(diff) == []
     assert expected_output_code == [output_code]
 
 
@@ -24,12 +23,11 @@ def test_diff_with_diff_python():
         "weights = [12, 15, 10]\nfor weight in weights:\n    print(weight)\n",
     ]
     expected_output_code = ["12\n151\n10\n"]
-    diff, output_code = dwys.diff(
+    diff, output_code, expected = dwys.diff(
         input_code=input_code,
         expected_output_code=expected_output_code,
         execution_command="python",
     )
-    assert list(diff) == ["--- \n", "+++ \n", "@@ -6 +5,0 @@\n", "-1"]
     assert expected_output_code != [output_code]
 
 
@@ -49,8 +47,8 @@ def test_diff_with_syntax_error_python():
 
 def test_diff_with_no_diff_R():
     input_code = ["a <- 4 + 4\nb <- a + 3\nprint(a + b)\n"]
-    expected_output_code = ["[1] 19\n"]
-    diff, output_code = dwys.diff(
+    expected_output_code = ["[1] 19"]
+    diff, output_code, expected = dwys.diff(
         input_code=input_code,
         expected_output_code=expected_output_code,
         execution_command="Rscript",
@@ -62,12 +60,20 @@ def test_diff_with_no_diff_R():
 def test_diff_with_diff_R():
     input_code = ["a <- 4 + 4\nb <- a + 3\nprint(a + b)\n"]
     expected_output_code = ["[1] 191\n"]
-    diff, output_code = dwys.diff(
+    diff, output_code, expected = dwys.diff(
         input_code=input_code,
         expected_output_code=expected_output_code,
         execution_command="Rscript",
     )
-    assert list(diff) == ["--- \n", "+++ \n", "@@ -7 +6,0 @@\n", "-1"]
+    assert list(diff) == [
+        "--- \n",
+        "+++ \n",
+        "@@ -5,4 +5,2 @@\n",
+        " 1",
+        " 9",
+        "-1",
+        "-\n",
+    ]
     assert expected_output_code != [output_code]
 
 
